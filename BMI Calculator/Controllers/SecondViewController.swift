820d425c6a89caec05456014e7d5bc476aa2cd0f
Averiser <averiser@gmail.com>
2021-09-22 13:17:10 +0300

    third commit - Create a Custom UIViewController Class

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by MyMacBook on 22.09.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
